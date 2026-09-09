485a148c39aff0d0b30dd9ad69f0c5029eea39c6
techdining01 <amooade12@gmail.com>
2026-09-09 11:17:01 +0100

    completed

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
+from web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
