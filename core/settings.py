041a5ae0d354534c585122906552364b0cbecc31
techdining01 <amooade12@gmail.com>
2026-09-09 11:38:31 +0100

    modified:   .env

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["logiclane-app.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["logiclane.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
