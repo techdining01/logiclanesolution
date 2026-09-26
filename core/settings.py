6bd07f9d78cb9068f6b66ece94de14ec97c24414
techdining01 <amooade12@gmail.com>
2026-09-26 05:09:51 +0100

    production

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'logiclanesolutions.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
